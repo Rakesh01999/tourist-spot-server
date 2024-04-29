e2ed9762c12ff93c5649369525e69f8facda6015
Rakesh Biswas <rbiswas01999@gmail.com>
2024-04-29 10:49:47 +0600

    server side successfully added

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uuibjb3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(uri);
 
